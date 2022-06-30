020df5247651939d5746a5f6d45e22113195e67e
Jla3eP <dimon.kornet@gmail.com>
2022-06-30 12:46:35 +0300

    changed pinger

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -14,6 +15,8 @@ func main() {
 	request, _ := http.NewRequest(http.MethodGet, "http://localhost:3500/eth/v1alpha1/beacon/chainhead", nil)
 	client := http.DefaultClient
 
+	pinger := sync.Once{}
+
 	var lastRespBody string
 	for {
 		<-ticker.C
@@ -21,11 +24,18 @@ func main() {
 		respBodyStr, _ := ioutil.ReadAll(resp.Body)
 
 		if string(respBodyStr) != lastRespBody {
+			if ping && lastRespBody != "" {
+				pinger.Do(func() {
+					go func() {
+						for i := 0; i < 15; i++ {
+							time.Sleep(80 * time.Millisecond)
+							fmt.Print("\a")
+						}
+					}()
+				})
+			}
 			lastRespBody = string(respBodyStr)
 			fmt.Println(lastRespBody + "\n")
-			if ping {
-				fmt.Print("\a")
-			}
 		}
 	}
 }
